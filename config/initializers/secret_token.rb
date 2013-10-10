9ca5766cb2ac460330fd9200309c0be949dcc52d
Peiying Wen <wenpeiying@gmail.com>
2013-10-09 23:44:31 -0700

    003 [Peiying] created home page; created rspec test suite with guard

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Wuhuaguo::Application.config.secret_key_base = '240813cf5fd8a61df5e064b20294496468834bbb2f9348c4b1d7bb0d2c780d6aa8a381dcb7653708d178a450ed957fd1fb461743a5dc3558fe0ae4d3442c27d9'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+Wuhuaguo::Application.config.secret_key_base = secure_token
