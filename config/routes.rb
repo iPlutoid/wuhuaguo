90778199c2b7d2e86a918ee499f48ac4f93bb3f4
youshoulong <blasphemymoon@gmail.com>
2013-10-18 17:16:54 +0800

    100 [Xiaolong] default questions page to index page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Wuhuaguo::Application.routes.draw do
 
 
   resources :questions
+  get '/' => 'questions#index'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
